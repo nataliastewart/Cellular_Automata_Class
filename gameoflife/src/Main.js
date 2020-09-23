5b8536f5f5538e35bc790cf1b5000b10de09754e
Natalia <nataliamirandac@hotmail.com>
2020-09-23 19:09:39 -0400

    Creates the Grid class

--- gameoflife/src/Main.js
+++ gameoflife/src/Main.js
@@ -1,9 +1,27 @@
 import React from "react";
 // import ReactDOM from "react-dom";
 
-class Main extends React.Component {
+class Grid extends React.Component {
   render() {
-    return <div> hi from MAIN</div>;
+    return <div>Hi from GRID</div>;
+  }
+}
+
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <Grid />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    );
   }
 }
 
