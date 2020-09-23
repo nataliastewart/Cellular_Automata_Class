bf71d72bc691d444bad2d815e62d208b9a1c68c4
Natalia <nataliamirandac@hotmail.com>
2020-09-23 19:31:46 -0400

    Creates Box Class - renders the box of grids in the browser

--- gameoflife/src/Main.js
+++ gameoflife/src/Main.js
@@ -1,24 +1,81 @@
 import React from "react";
 // import ReactDOM from "react-dom";
+import "./Main.css";
+
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col);
+  };
+
+  render() {
+    return (
+      <div
+        className={this.props.boxClass}
+        id={this.props.id}
+        onCLick={this.selectBox}
+      ></div>
+    );
+  }
+}
 
 class Grid extends React.Component {
   render() {
-    return <div>Hi from GRID</div>;
+    const width = this.props.cols * 16;
+    var rowsArr = [];
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; ++i) {
+      for (var j = 0; j < this.props.rows; ++j) {
+        let boxId = i + "_" + j;
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          //pushing a bunch of ox into the array
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+    return (
+      //calling the array of boxes down here {rowsArr}
+      <div className="grid" style={{ width: width }}>
+        {rowsArr}
+      </div>
+    );
   }
 }
 
 class Main extends React.Component {
   constructor() {
     super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
     this.state = {
       generation: 0,
+      //array big as rows variable and fill that with map. cretae another array and each element in that array is false. start grid cell turn off
+      gridFull: Array(this.rows)
+        .fill()
+        .map(() => Array(this.cols).fill(false)),
     };
   }
   render() {
     return (
+      /* passing variables as props to Grid component */
       <div>
         <h1>Hello</h1>
-        <Grid />
+
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
         <h2>Generations: {this.state.generation}</h2>
       </div>
     );
